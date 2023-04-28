db58ba0f28848a184812f10fdc51643567955e16
MonoChyvo <mescal.13@proton.me>
2023-04-27 22:24:29 -0600

    first commit

---
+++ src/components/Content.jsx
@@ -0,0 +1,21 @@
+import { ItemList } from './ItemList';
+
+export const Content = ({ items, handleCheck, handleDelete }) => {
+
+  return (
+    <main>
+      {items.length ? (
+        <ItemList
+          items={items}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete} />
+      ) : (
+        <p style={{ marginTop: '2rem' }}>Your list is empty.</p>
+      )}
+    </main>
+  );
+};
+
+
+
+
---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export const Footer = ({ length }) => {
+
+    return (
+        <footer>
+            <p>{length} List {length === 1 ? 'item' : 'items'} </p>
+        </footer>
+    );
+};
